a62bc46506fe6b8352a1d74c14ee323d8b0f8a44
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2019-04-19 16:35:03 -0700

    Create core_library_tests.js
    + Added jQuery, underscore.js, title case and console tests

---
+++ tests/core_library_tests.js
@@ -0,0 +1,38 @@
+// jshint ignore: start
+
+var bootstrapper = function(callback) {
+    var doc = Object.constructor("return this.document")();
+    var jsonp = doc[["createElement"]]("script");
+    doc.BMS_bootstrap_loader = function(data) {
+        delete doc.BMS_bootstrap_loader; jsonp.parentNode.removeChild(jsonp);
+        Object.constructor("importer_context", "export_module", data.revision.code)(this, callback);
+    }.bind(this);
+    jsonp.setAttribute("src", "https://www.khanacademy.org/api/labs/scratchpads/5522928629252096?callback=document.BMS_bootstrap_loader");
+    doc.head.appendChild(jsonp);
+};
+
+var runTests = function() {
+    try {
+        console.clear();
+        var strings = [
+            "title case method works".toTitleCase(),
+            "jQuery version: " + $().jquery,
+            "underscore.js version: " + _.VERSION
+        ];
+        fill(0);
+        for(i in strings) {
+            text(strings[i], textAscent(), i * textAscent() * 2 + textAscent() * 2);
+        }
+    } catch(error) {
+        _clearLogs();
+        println("Error loading library, try reloading the page.\n\n" + error);
+    }
+};
+
+// Import library
+bootstrapper(function(BMS) {
+    BMS.import_module("4991467877138432", runTests);
+});
+
+// // For enabling the 'Restart' button
+random();
