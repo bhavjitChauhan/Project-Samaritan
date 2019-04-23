078640ac5c58b2c0a5adfd3d6b6966e2545b3525
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2019-04-22 20:53:43 -0700

    Tweak structure
    + Moved `bootstrapper` function
    + Used `let` and `const` statements in place of `var`
    - Removed app version and user agent; most likely to be in an object in the core library

--- core.js
+++ core.js
@@ -1,16 +1,5 @@
 // jshint ignore: start
 
-var bootstrapper = function(callback) {
-    var doc = Object.constructor("return this.document")();
-    var jsonp = doc[["createElement"]]("script");
-    doc.BMS_bootstrap_loader = function(data) {
-        delete doc.BMS_bootstrap_loader; jsonp.parentNode.removeChild(jsonp);
-        Object.constructor("importer_context", "export_module", data.revision.code)(this, callback);
-    }.bind(this);
-    jsonp.setAttribute("src", "https://www.khanacademy.org/api/labs/scratchpads/5522928629252096?callback=document.BMS_bootstrap_loader");
-    doc.head.appendChild(jsonp);
-};
-
 // System object
 const System = function() {
     this.init();
@@ -21,7 +10,7 @@ System.prototype = {
         console.time("System Initialize");
         
         // Basic system information
-        let info = {
+        this.info = {
             name: "Project Samaritan",
             version: "0.0.1",
             author: "Aliquis"
@@ -29,33 +18,10 @@ System.prototype = {
         
         // Log system information to console
         console.group("");
-        console.info("%c%s v%s", "font-weight:bold;", info.name, info.version);
-        console.info("(c) %d %s. Most rights reserved, I think...", year(), info.author);
+        console.info("%c%s v%s", "font-weight:bold;", this.info.name, this.info.version);
+        console.info("(c) %d %s. Most rights reserved, I think...", year(), this.info.author);
         console.info("");
         console.groupEnd();
-        
-        // Client information
-        let window = (function () {
-            return this;
-        })();
-        let appVersion = window.navigator.appVersion
-        console.debug(appVersion);
-        let userAgent = window.navigator.userAgent;
-        console.debug(userAgent);
-        // let browser = (function() {
-        //     var browsers = ["MSIE", "Firefox", "Safari", "Chrome", "Opera"];
-        //     for (i = browsers.length - 1; i > -1 && this.userAgent.indexOf(browsers[i]) === -1; i--) {}
-        //     return browsers[i];
-        // })();
-        
-        info.client = {
-            "window": window,
-            "appVersion": appVersion,
-            "userAgent": userAgent
-        };
-        
-        // Make info public key
-        this.info = info;
         
         console.timeEnd("System Initialize");
     },
@@ -66,13 +32,24 @@ System.prototype = {
     }
 };
 
-var __requirements__ = {
-    "": "#5219990839590912"
+const bootstrapper = function(callback) {
+    let doc = Object.constructor("return this.document")();
+    let jsonp = doc[["createElement"]]("script");
+    doc.BMS_bootstrap_loader = function(data) {
+        delete doc.BMS_bootstrap_loader; jsonp.parentNode.removeChild(jsonp);
+        Object.constructor("importer_context", "export_module", data.revision.code)(this, callback);
+    }.bind(this);
+    jsonp.setAttribute("src", "https://www.khanacademy.org/api/labs/scratchpads/5522928629252096?callback=document.BMS_bootstrap_loader");
+    doc.head.appendChild(jsonp);
+};
+
+const __requirements__ = {
+    "core": "#5219990839590912"
 }
 
 bootstrapper({
     done: function(BMS, methods) {
-        var system = new System();
+        let system = new System();
         draw = function() {
             if(!system.booted) {
                 system.boot();
