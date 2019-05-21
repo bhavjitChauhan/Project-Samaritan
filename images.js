ac9c6c76cc9a7233f783b1aa89699cb8d0ceaf3c
Bhavjit Chauhan <25716723+bhavjitChauhan@users.noreply.github.com>
2019-05-20 21:00:58 -0700

    Polished code (#3)
    * Format core.js
    
    + Added `clearMemory` variable
    + Added `ENV` variable
    + Added startup verbose screen
    + `testApplication` will now run after system boot
    + Formatted code
    
    * Add export variables and format code
    
    + Added `colors` and `fonts` objects to exports
    + Added `assert` function
    + Added environment check for `copyToClipboard`
    + Added default values for parameters in documentation comments
    + Export objects now uses `Object.assign` to merge multiple objects for better organization
    + Formatted code
    
    * Format core-library-tests.js
    
    + Added `ENV` variable
    + Moved `bootstrapper` function with `__requirements__` object
    + Moved `console.clear` outside of `run` function to avoid core library messages from getting cleared
    
    * Format applications.js and update test application
    
    + Added `NAME`, `VERSION` and `AUTHOR` constants
    + Added standalone program screen
    + Changed `testApplication` to be more visually appealing and utilize more features
    
    * Format images.js and update test image
    
    + Added `NAME`, `VERSION` and `AUTHOR` constants
    + Added standalone program screen
    + Changed `testImage` to be more visually appealing

--- images.js
+++ images.js
@@ -1,42 +1,60 @@
 // jshint ignore: start
+const NAME = "Project Samaritan Images Library",
+	VERSION = "0.0.2",
+	AUTHOR = "Aliquis";
 
 console.time("Import Images");
-
+// Image Object {
 const Image = function(name, code) {
-    this.name = name || "Unknown";
-    if(typeof code !== "function") {
-        console.error("Image '" + this.name + "' has invalid draw code.");
-        return;
-    }
-    this.load = function() {
-        background(0, 0);
-        pushMatrix();
-        pushStyle();
-        code();
-        popStyle();
-        popMatrix();
-        this.image = get();
-        this.loaded = true;
-    };
-    this.draw = function(x, y) {
-        if(!this.loaded) {
-            console.warn("Attempted to draw image '" + this.name + "' before initialization.");
-            this.load();
-        }
-        image(this.image, x, y);
-    };
+	this.name = name || "Unknown";
+	if (typeof code !== "function") {
+		console.error("Image '" + this.name + "' has invalid draw code.");
+		return;
+	}
+	this.load = function() {
+		background(0, 0);
+		pushMatrix();
+		pushStyle();
+		code();
+		popStyle();
+		popMatrix();
+		this.image = get();
+		this.loaded = true;
+	};
+	this.draw = function(x, y) {
+		if (!this.loaded) {
+			console.warn("Attempted to draw image '" + this.name +
+				"' before initialization.");
+			this.load();
+		}
+		image(this.image, x, y);
+	};
 };
-
+// }
+// Images {
 const testImage = new Image("Test", function() {
-    fill(0, 111, 222);
-    rect(0, 0, 100, 100, 15);
+	translate(25, 30);
+    noStroke();
+    fill(RED);
+    quad(0, 0, -25, -15, 0, -30, 25, -15);
+    fill(GREEN);
+    quad(0, 0, -25, -15, -25, 15, 0, 30);
+    fill(BLUE);
+    quad(0, 0, 25, -15, 25, 15, 0, 30);
 });
-
 var images = [testImage];
-
+// }
+// Exporting {
 let export_module;
-if(export_module) {
-    console.log("Importing images object...")
-    export_module(images);
-    console.timeEnd("Import Images");
+if (export_module) {
+	console.log("Importing images object...")
+	export_module(images);
+	console.timeEnd("Import Images");
+} else {
+    background(255);
+    fill(0);
+    textAlign(CENTER);
+    textFont(createFont("monospace"), 15);
+    text(NAME + "\nVersion " + VERSION, width / 2, height / 2);
 }
+// }
